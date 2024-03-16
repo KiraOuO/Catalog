be10794fb6bfede8c2a1996353f10e7562d9288c
Kira <kirayekasova@gmail.com>
2024-03-16 02:49:10 -0400

    change product page, add cart, order history, order details, registration

--- js/index.js
+++ js/index.js
@@ -6,3 +6,25 @@ document.addEventListener('DOMContentLoaded', function () {
         dropdownItems.classList.toggle('open');
     });
 });
+
+
+const quantityInputs = document.querySelectorAll('.quantity-count');
+const increaseBtns = document.querySelectorAll('.plus');
+const decreaseBtns = document.querySelectorAll('.minus');
+let minimun_value = 0;
+
+increaseBtns.forEach((btn, index) => {
+    btn.addEventListener('click', () => {
+        quantityInputs[index].stepUp();
+    });
+});
+
+decreaseBtns.forEach((btn, index) => {
+    btn.addEventListener('click', () => {
+        if (quantityInputs[index].value > minimun_value) {
+            quantityInputs[index].stepDown();
+        }
+    });
+});
+
+
