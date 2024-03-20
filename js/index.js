80144e27cffece080058f8815a9d1348036bc807
Kira <kirayekasova@gmail.com>
2024-03-20 18:36:48 -0400

    fix responsive navbar

--- js/index.js
+++ js/index.js
@@ -52,5 +52,36 @@ document.querySelector('.burger-menu').addEventListener('click', function(event)
 });
 
 
+const blackBack = document.querySelectorAll('.black-back');
+const searchButton = document.querySelector('.search-mobile-button img'); // Получаем ссылку на изображение кнопки поиска
+
+document.querySelector('.search-mobile-button').addEventListener('click', function() {
+    let burgerMenu = document.getElementById('search_menu');
+    if (burgerMenu.style.display === 'flex') {
+        burgerMenu.style.display = 'none';
+        searchButton.src = 'img/search.png'; // Возвращаем исходную иконку поиска
+        blackBack.forEach(back => back.style.display = 'none'); // Скрываем черный фон для всех элементов
+        document.body.classList.remove('search-menu-open');
+    } else {
+        burgerMenu.style.display = 'flex';
+        searchButton.src = 'img/x.svg'; // Меняем иконку на крестик
+        blackBack.forEach(back => back.style.display = 'block'); // Показываем черный фон для всех элементов
+        document.body.classList.add('search-menu-open');
+    }
+});
+
+document.querySelector('.search-menu').addEventListener('click', function(event) {
+    if (event.target === this) {
+        this.style.display = 'none';
+        searchButton.src = 'img/search.png'; // Возвращаем исходную иконку поиска
+        document.body.classList.remove('search-menu-open');
+        blackBack.forEach(back => back.style.display = 'none'); // Скрываем черный фон для всех элементов
+    }
+});
+
+
+
+
+
 
 
